0afb213755654b6a6e223129b00ed20c28aedefd
richintheflow <richardg@posteo.net>
2022-04-12 16:29:54 +0200

    Add playerSelection function in script.js
    Return message of who wins or looses in different sitautions
    into /rock-paper-scissors

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
+// The computer choose randomly between rock, paper or scissors
 function computerPlay() {
-  let randomRockPaperScissors = Math.floor(Math.random() * 3 );
+  const randomRockPaperScissors = Math.floor(Math.random() * 3 );
     if (randomRockPaperScissors === 0 ) {
-      console.log("rock");
+      return "rock";
     } else if (randomRockPaperScissors === 1) {
-      console.log("paper");
+      return "paper";
     } else if (randomRockPaperScissors === 2 ) {
-      console.log("scissors");
+      return "scissors";
     }
 }
+//const computerSelection = computerPlay();
+// console.log(computerSelection);
 
-computerPlay()
+
+
+function playRound(playerSelection, computerSelection) {
+  // player wins if
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "player wins, rock beats scissors";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "player wins, scissors beats paper";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "player wins, papers beats rock";
+  } 
+  // player looses if
+  else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "player looses, rock beats scissors";
+  }   else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "player looses, scissors beats paper";
+  }   else if (playerSelection == "rock" && computerSelection == "paper") {
+    return "player looses, paper beats rock";
+  } 
+  // player & computer equal if
+  else if (playerSelection == "scissors" && computerSelection == "scissors") {
+    return "scissors vs scissors, nobody wins, play another round";
+  }   else if (playerSelection == "paper" && computerSelection == "paper") {
+    return "paper vs paper, nobody wins, play another round";
+  }   else if (playerSelection == "rock" && computerSelection == "rock") {
+    return "rock vs rock, nobody wins, play another round";
+  }  
+}
+const playerSelection = prompt("Choose between rock, paper and scissors");
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+
+function game () {
+  for (let i = 0; i < 5; i++) {
+    //code
+ }
+}
